d9d09d89fe933a0a8419069661258f45f75ecf3a
Diego Viero <d.viero@student.tudelft.nl>
2022-04-29 17:25:53 +0200

    refactor question and answer component

--- frontend/src/components/Answer.jsx
+++ frontend/src/components/Answer.jsx
@@ -2,16 +2,17 @@ import React from "react";
 
 export const Answer = (props) => {
 
+    const inputStyle = "appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer";
 
     return (
-      <div className="flex w-fit mt-5 child:mx-1 ">
-        <h3 className="pr-10">Strongly disagree</h3>
-        <input className="w-10" type="radio" value="1" name={props.questionNumber} /> 
-        <input className="w-10" type="radio" value="2" name={props.questionNumber} /> 
-        <input className="w-10" type="radio" value="3" name={props.questionNumber} checked/> 
-        <input className="w-10" type="radio" value="4" name={props.questionNumber} /> 
-        <input className="w-10" type="radio" value="5" name={props.questionNumber} /> 
-        <h3 className="pl-10">Strongly agree</h3>
+      <div className="flex justify-center w-fit mt-5 space-x-7 ">
+        <h3 className="">Strongly disagree</h3>
+        <input className={inputStyle} type="radio" value="1" name={props.questionNumber} /> 
+        <input className={inputStyle} type="radio" value="2" name={props.questionNumber} /> 
+        <input className={inputStyle} type="radio" value="3" name={props.questionNumber} /> 
+        <input className={inputStyle} type="radio" value="4" name={props.questionNumber} /> 
+        <input className={inputStyle} type="radio" value="5" name={props.questionNumber} /> 
+        <h3 className="">Strongly agree</h3>
       </div>
     )
 }
--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -5,7 +5,7 @@ import { Answer } from './Answer';
 export const Question = (props) => {
 
     return (
-        <div className='flex-col py-10'>
+        <div className='flex flex-col py-10 items-center'>
             <h1 className='text-blue-500 text-center text-2xl'>{props.text}</h1>
             <Answer questionNumber={props.questionNumber}/>
         </div>
