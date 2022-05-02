4c054d2b822c155e410c1fcc8bdc7f7a55aeac55
Kenzo Boudier <K.T.C.Boudier@student.tudelft.nl>
2022-05-02 15:53:57 +0200

    Added structure to the back end for storing user and answers

--- backend/psychology.py
+++ backend/psychology.py
@@ -10,3 +10,7 @@ def pers_calc(val_answers):
 
 def calculations(val_answers, pers_answers):
     return val_calc(val_answers), pers_calc(pers_answers)
+
+def split_data(answers):
+    # TODO: split received answers from frontend
+    return 0, 0
--- backend/service.py
+++ backend/service.py
@@ -1,8 +1,14 @@
-def store_answers(user_id, val_answers, pers_answers, values, personalities):
-    # TODO: store all data in our database
-    return "Success storing"
+def store_answers(user_id, answers):
+    for i in range(len(answers)):
+        store_answer(user_id, answers[i], i + 1)
+    return "Success storing all Answers"
 
 
-def store_calculations(user_id, values, personalities):
+def store_user(user_id, values, personalities, batch):
     # TODO: store all data in our database
-    return "Success storing"
+    return "Success storing user"
+
+
+def store_answer(user_id, answer, question_number):
+    # TODO: store one answer
+    return "Success storing answer"
