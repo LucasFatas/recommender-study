67dfa8a43602dcaefb094603f9f9ef025ff4b304
Diego Viero <d.viero@student.tudelft.nl>
2022-05-02 16:35:19 +0200

    add unique index to each question and pages are now not hard-coded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,54 +6,40 @@ import {
   Navigate
 } from "react-router-dom";
 
+import questions from './util/questions.json';
 
 const App = () => {
 
-  /*
-    When receiving all the questions split them in arrrays of 
-    questions : 
-    const chunkSize = 3;
+  const questionsList = questions.questions;
 
-    const [q1, q2, q3, q4] = arr.map((e, i) => { 
-        return i % chunkSize === 0 ? arr.slice(i, i + chunkSize) : null; 
-    }).filter(e => { return e; });
+  const questionsPerPage = 5
+  
+  const questionMatrix = questionsList.map((e, i) => {
+    return i % questionsPerPage === 0 ? questionsList.slice(i, i + questionsPerPage).map((e, idx) => [e, i + idx]) : null;
+  }).filter(e => { return e; });
 
-  */
+  const lastQuestionIdx = questionMatrix.length;
 
-  const questions = [
-    'What is your name?',
-    'How old are you?',
-    'Where are you from?'
-  ]
-
-  const questions2 = [
-    'How much do you relate to this?',
-    'Do you believe that you exist?',
-    'Do you believe in the existence of god?'
-  ]
-
-  const questions3 = [
-    'Bla bla bla question?',
-    'words words words question?',
-    'sentence sentence question?'
-  ]
+  console.log(questionMatrix);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate replace to="/page0"/>} />
-        {/* Page 0 */}
-        <Route path="/page0" element={ 
-          <Page questions={questions} nextPage={1}/>
-        }/>
-        {/* Page 1 */}
-        <Route path="/page1" element={
-          <Page questions={questions2} prevPage={0} nextPage={2} />
-        } />
-        {/* Page 2 */}
-        <Route path="/page2" element={
-          <Page questions={questions3} prevPage={1} />
-        } />
+
+        {
+          questionMatrix.map((questions, idx) => (
+            <Route path={`/page${idx}`} key={idx} element={
+              <Page 
+                pageNumber={idx}
+                questions={questions} 
+                prevPage={idx === 0 ? undefined : idx - 1} 
+                nextPage={idx === lastQuestionIdx - 1 ? undefined : idx + 1}
+                showSubmit={idx === lastQuestionIdx - 1}
+              />
+            }/>
+          ))
+        }
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/Page.jsx
+++ frontend/src/components/Page.jsx
@@ -18,15 +18,18 @@ export const Page = (props) => {
         </Link>
     );
 
+    const submitButton = (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">submit</button>
+    )
 
     return (
         <div className='grid place-items-center'>
-            {props.questions.map((text, index) => 
+            {props.questions.map(([text, index]) => 
                 <Question text={text} questionNumber={index} key={index}/>
             )}
             <div className="flex">
                 { displayIfDefined(props.prevPage, linkToPreviousPage) }
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">submit</button>
+                { props.showSubmit ? submitButton : <></>}
                 { displayIfDefined(props.nextPage, linkToNextPage) }
             </div>
         </div>
