74ecefcfa1b77d38f3d8657def46558f0cef64df
Daniel Puente Barajas <D.PuenteBarajas@student.tudelft.nl>
2022-05-03 14:01:33 +0200

    add comments and clean code.

--- backend/controller.py
+++ backend/controller.py
@@ -10,10 +10,9 @@ app = Flask(__name__)
 
 
 # Beginning of endpoint methods
-
-# Connection between frontend and backend test
+# Spoof endpoint to test connection.
 @app.route("/")
-def mainConnection():
+def main_connection():
     return "Connection is working"
 
 
@@ -29,28 +28,46 @@ def save_answer():
     # TODO: store answers into our database
     answers = []
 
+    # Format answers retrieved from frontend into our database format.
+    # Format: UserId, question number, answer.
+
+    # Personality answers formatting
     for index, answer in enumerate(data['personality_answers']):
         answers.append((data['user'], index, answer))
 
+    # Value answers formatting
     for index, answer in enumerate(data['value_answers']):
         answers.append((data['user'], len(data['personality_answers']) + index, answer))
 
+    # Add the newly formatted answers to our database.
     add_answers(answers)
 
+    # Calculate value and personality scores.
+    value, personality = calculations(data['value_answers'], data['personality_answers'])
+
     # Process successful, return results for frontend to show to the user.
-    #return jsonify(values=values, personalities=personalities)
-    return "It Works"
+    return jsonify(values=value, personalities=personality)
 
 
+# Handle the Spotify login and access code retrieval.
 @app.route('/callback')
-def spotifyLogIn():
+def spotify_log_in():
     try:
+        # Retrieve the access token after user is logged in.
         access_token = get_access_token(request.args['code'])
+
+        # Find top songs of the user.
         songs = json.dumps(get_top_songs(access_token))
-        add_user(1) # Batch Number hardcoded for now
+
+        # Store the user in the database.
+        add_user(1)  # Batch Number hardcoded for now
+
+        # TODO: add songs to the database. (according to user)
         # add_songs(songs)
+
         return songs
     except AuthorizationException as e:
+        # Exception handling in case there is an error.
         response = jsonify({'message': str(e)})
         return response, 401
 
