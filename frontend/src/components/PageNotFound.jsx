2f390c16badddbd88bddb65a669a14d22f51fa59
Diego Viero <d.viero@student.tudelft.nl>
2022-05-04 16:50:06 +0200

    add error page for undefined paths

---
+++ frontend/src/components/PageNotFound.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const PageNotFound = (props) => {
+
+    return (
+        <div className="h-3/4 w-screen flex flex-col justify-center content-center items-center absolute z-0">
+            <h1 className="uppercase p-10 text-xl font-black">
+                We're sorry, but something went wrong.
+            </h1>
+            <Link to={props.redirectPath}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-lg text-white font-bold py-2 px-4 rounded-full ">
+                    Back to homepage
+                </button>
+            </Link>
+        </div>
+    )
+}
