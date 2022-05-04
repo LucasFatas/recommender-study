c67a186f6c32294662d45841063ee7ff5e11d5a6
Diego Viero <d.viero@student.tudelft.nl>
2022-05-04 15:43:04 +0200

    general refactor and bug fix
    refactor add radio button component
    
    
    refactor change invalid values from undefined to false
    
    
    implement answers memorization throughout pages
    
    
    fix bug regarding navigation
    
    After answering all questions in one page, going to the next page and then going back to the original one, the "next" button wasn't showing.
    reafactor move questionnaire components into dedicated folder
    
    
    refactor pages and questions in App
    
    - Pages index now start from 1 instead of 0
    - questionsPerPage variable moved to json file
    refator button visibility
    
    now certain buttons appear opaque instead of invisible.
    Fix bug where a user could still go to the next page even though the button was disabled

---
+++ frontend/src/components/questionnaire/QuestionnairePage.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState, useEffect, useMemo } from "react";
+
+import { Answer } from './Answer';
+import { Buttons } from "./Buttons";
+
+export const QuestionnairePage = (props) => {
+  
+  //boolean value to check if all answers in the current page have been answered.
+  const [ answered, setAnswered ] = useState(false);
+  const questionsNumber = useMemo(() => [...props.questions.map(x => x[1] + 1)], [props.questions]);
+  
+  useEffect(() => 
+    setAnswered(questionsNumber.every(x => props.answers.has(x))),
+    [questionsNumber, props.answers] //parameters that, if changed, trigger the function above
+  );
+
+  const updateAnswers = (e, setSelected, questionNumber, value) => {
+    //Set solution in answers map
+    props.setAnswers(props.answers.set(questionNumber + 1, e.target.value));
+    
+    //Set selected radio button in answer
+    setSelected(props.answers.get(questionNumber + 1) === value);
+    
+    //Change boolean value
+    setAnswered(questionsNumber.every(x => props.answers.has(x)));
+  }
+
+  const handleNext = () => {
+    const nextQuestionsNumber = questionsNumber.map(x => x + questionsNumber.length);
+    setAnswered(nextQuestionsNumber.every(x => props.answers.has(x)))
+  }
+
+  return (
+    <div className='grid place-items-center'>
+      {props.questions.map(([text, index]) => 
+        <div className='flex flex-col py-10 items-center' key={index}>
+          <h1 className='text-blue-500 text-center text-2xl'>{text}</h1>
+          <Answer 
+            answers={props.answers} 
+            questionNumber={index}  
+            onChange={updateAnswers}
+          />
+        </div>
+      )}
+      <Buttons {...props} answered={answered} onNext={handleNext}/>
+    </div>
+  );
+}
---
+++ frontend/src/components/questionnaire/RadioButton.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const inputStyle = "appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer";
+
+export const RadioButton = ({ answers, value, questionNumber, onChange }) => {
+
+  const checkPreviousAnswers = () => answers.get(questionNumber + 1) === value.toString()
+  
+  const [selected, setSelected] = useState(checkPreviousAnswers());
+
+  return (
+    <input 
+      type="radio" 
+      className={inputStyle} 
+      value={value}
+      name={questionNumber}
+      onChange={(e) => onChange(e, setSelected, questionNumber, value.toString())}
+      defaultChecked={selected}
+    /> 
+  )
+
+}
