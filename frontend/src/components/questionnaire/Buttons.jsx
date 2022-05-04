e38df01449c401b212db6ccc1e06566527f8d479
Nathaniël De Leeuw <N.DeLeeuw@student.tudelft.nl>
2022-05-04 17:21:22 +0200

    WIP submit button send answer to back-end

--- frontend/src/components/questionnaire/Buttons.jsx
+++ frontend/src/components/questionnaire/Buttons.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import APIService from '../APIService';
 
 const buttonStyle = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ";
 const buttonStyleDisabled = " select-none text-transparent fonted bg-transparent hover:bg-transparent py-2 px-4 rounded-full ";
@@ -23,7 +24,7 @@ export const Buttons = (props) => {
         </button> 
       </Link>
       
-      <button {...setStyleAndDisabled(props.showSubmit, props.answered, buttonStyleOpaque)}>
+      <button {...setStyleAndDisabled(props.showSubmit, props.answered, buttonStyleOpaque)} onClick={() => APIService.sendAnswer(props.answers)} >
         Submit
       </button>
 
@@ -34,4 +35,4 @@ export const Buttons = (props) => {
       </Link>
     </div>
   );
-}
+}   
