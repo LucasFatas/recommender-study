4dfe574fb9dbf876cacd65384d6af6e6ba8d5060
LucasFatas <69412382+LucasFatas@users.noreply.github.com>
2022-05-06 15:59:46 +0200

    Adding redirect user to frontend after spotify log-in

--- backend/src/controller.py
+++ backend/src/controller.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from src.service import add_user, add_answers, DatabaseException
 from src.psychology import calculations
 from src.spotify import get_access_token, get_top_songs, AuthorizationException
 import json
-
+from werkzeug.wrappers.response import Response
 
 app = Flask(__name__)
+
+frontend_url = "http://www.localhost.com/3000/"
 
 
 # Beginning of endpoint methods
@@ -22,7 +24,7 @@ def save_answer():
 
     # Calculations for the personality and values.
     # TODO: implement value and personality calculations
-    #values, personalities = calculations(answers)
+    # values, personalities = calculations(answers)
 
     # TODO: store answers into our database
     answers = []
@@ -64,22 +66,21 @@ def spotify_log_in():
         songs = json.dumps(get_top_songs(access_token))
 
         # Store the user in the database.
-        add_user(1)  # Batch Number hardcoded for now
+        userID = add_user(1)  # Batch Number hardcoded for now
 
         # TODO: add songs to the database. (according to user)
         # add_songs(songs)
+        return redirect(frontend_url + "page1?userID = " + userID, 200)
 
-        return songs
-    
     except AuthorizationException as e:
         # Exception handling in case there is an authorization error.
-        response = jsonify({'message': str(e)})
-        return response, 401
+        response = Response(str(e))
+        return redirect(frontend_url + "page1")
 
     except DatabaseException as e:
         # Exception handling in case there is a database error.
-        response = jsonify({'message': str(e)})
-        return response, 502
+        response = Response(str(e))
+        return redirect(frontend_url + "page1")
 
 
 def create_app():
@@ -90,5 +91,3 @@ def create_app():
 
 if __name__ == "__main__":
     create_app()
-
-
