71725bba1c0521ece64dc942def0fabba38bbebf
Diego Viero <d.viero@student.tudelft.nl>
2022-05-06 15:35:41 +0200

    preliminary connection with backend working

--- frontend/src/API/APIService.js
+++ frontend/src/API/APIService.js
@@ -2,18 +2,17 @@ export default class APIService{
     // Send the answers
     static sendAnswer(body){
         
-        const obj = Object.fromEntries(body);
-        console.log(obj);
+        const mapToObject = Object.fromEntries(body);
 
         return fetch(`http://localhost:5000/saveAnswer`,{
-             method :'POST',
-             crossDomain : true,
-             headers : {
+            method :'POST',
+            mode : 'no-cors',
+            headers : {
                 'Content-Type':'application/json'
             },
-            body: JSON.stringify(obj),
+            body: JSON.stringify(mapToObject),
         })
-        .then(response => response.json())
+        .then(response => {console.log(response)})
         .catch(error => console.log(error))
     }
 
