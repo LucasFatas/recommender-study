22142ca6d760d88111dbadecc768d6729413457d
Diego Viero <d.viero@student.tudelft.nl>
2022-05-09 16:09:54 +0200

    add object to store recommender results

--- frontend/src/components/recommender/Playlist.jsx
+++ frontend/src/components/recommender/Playlist.jsx
@@ -1,47 +1,27 @@
 import React from "react";
 import { StarRating } from "./StarRating";
 
-export const Playlist = ({name, starSize}) => {
-
+export const Playlist = (props) => {
 
 	return (
 		<div>
-			<h2>{ name }</h2>
+			<h2>{ props.name }</h2>
 			<div>
-				<h3>Song 1</h3>
-				<StarRating
-				starSize={starSize}
-				/>
+				{[...Array(5)].map((e, idx) => (
+					<div key={idx}>
+						<h3>Song {idx + 1}</h3>
+						<StarRating 
+							startStyle="text-xl" 
+							playlist={props.name} 
+							song={idx} 
+							key={idx}
+							{...props}
+						/>
+					</div>
+				))}
 			</div>
 			
-			<div>
-				<h3>Song 2</h3>
-				<StarRating
-				starSize={starSize}
-				/>
-			</div>
-
-			<div>
-				<h3>Song 3</h3>
-				<StarRating
-				starSize={starSize}
-				/>
-			</div>
-			<div>
-				<h3>Song 4</h3>
-				<StarRating
-				starSize={starSize}
-				/>
-			</div>
-			<div>
-				<h3>Song 5</h3>
-				<StarRating
-				starSize={starSize}
-				/>
-			</div>
-			<StarRating
-				starSize={'text-2xl text-center'}
-				/>
+			<StarRating startStyle='text-2xl text-center' playlist={props.name} {...props}/>
 		</div>
 	)
 }
--- frontend/src/components/recommender/Recommender.jsx
+++ frontend/src/components/recommender/Recommender.jsx
@@ -1,31 +1,52 @@
 import React from "react";
+import { useState } from "react";
 import { Playlist } from "./Playlist";
 
 export const Recommender = () => {
+
+	const [ratingsFilled, setRatingsFilled] = useState(false);
+	const [ratings, setRatings] = useState({
+		ratings : {
+			random : {
+				playlist: 0, 
+				songs: [0, 0, 0, 0, 0]
+			},
+			personality : {
+				playlist: 0,
+				songs: [0, 0, 0, 0, 0]
+			},
+			values : {
+				playlist: 0,
+				songs: [0, 0, 0, 0, 0]
+			},
+		},
+		comment : ""
+	})
+
+	//TODO add textbox content to object comment
+
+	const buttonStyles = {
+		active : "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ",
+		disabled : "select-none text-transparent fonted bg-transparent hover:bg-transparent py-2 px-4 rounded-full",
+		inactive : "select-none bg-blue-300 text-white font-bold py-2 px-4 rounded-full "
+	}
 
 
 	return (
 		<div className='grid place-items-center'>
 			<div className="flex justify-center w-fit mt-10 space-x-7 ">
-				{/* <div className=' w-70  border-2 border-sky-500 '> */}
-						
-				<Playlist
-						name="Playlist 1"
-						starSize="text-xl"
-				/>
-				<Playlist
-				name="Playlist 2"
-				starSize="text-xl"
-				/>
-				<Playlist
-				name="Playlist 2"
-				starSize="text-xl"
-				/>
-				
-					
+				{['random', 'personality', 'values'].map(name => (
+					<Playlist
+						name={name}
+						setRatings={setRatings}
+						ratings={ratings}
+						key={name}
+						setRatingsFilled={setRatingsFilled}
+					/>
+				))}
 			</div>
-			
-			<textarea autofocus required placeholder="type your message" maxlength="180"  cols="50" rows="6" wrap="hard" className=' mt-10 border-2 border-sky-500 '/>
+			<textarea className='mt-10 border-2 border-sky-500' placeholder="type your message" maxLength="180" cols="50" rows="6" wrap="hard"/>
+			<button className={ratingsFilled ? buttonStyles.active : buttonStyles.inactive} disabled={!ratingsFilled}>Submit</button>
 		</div>
 	)
 }
