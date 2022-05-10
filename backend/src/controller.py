2d51db75a4dcc1a5a7a71554297875e928d976ba
LucasFatas <69412382+LucasFatas@users.noreply.github.com>
2022-05-10 12:58:06 +0200

    Adding api endpoint for saving ratings

--- backend/src/controller.py
+++ backend/src/controller.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify, redirect
-from src.service import add_user, add_answers, DatabaseException, get_top_songs, add_top_songs
+from src.service import add_user, add_answers, DatabaseException, get_top_songs, add_top_songs, add_playlist_ratings, add_song_ratings
 from src.psychology import calculations
 from src.spotify import get_access_token, get_top_songs_api, AuthorizationException
 import json
 from flask_cors import CORS
+from src.Entities.SongRating import SongRating
+from src.Entities.PlaylistRating import PlaylistRating
+
 from werkzeug.wrappers import Response
 
 app = Flask(__name__)
@@ -63,6 +66,28 @@ def retrieve_top_songs():
         data = request.get_json(force=True)
         top_songs = get_top_songs(data['userId'])
         return json.dumps(top_songs)
+    except DatabaseException as e:
+        # Exception handling in case there is a database error.
+        return redirect(frontend_url + "/error/database")
+
+
+@app.route('/ratings/add', methods=["POST"])
+def save_ratings():
+    try:
+        data = request.get_json(force=True)
+        userId = data['userId']
+        songRatings = []
+        for songRating in data['songRatings']:
+            songRatings.append(SongRating(userId, songRating['matchedUserId'], songRating['spotify_url'],
+                                          songRating['rating']))
+        add_song_ratings(songRatings),
+
+        playlistRatings = []
+        for playlistRatings in data['playlistRatings']:
+            playlistRatings.append(SongRating(userId, playlistRatings['matchedUserId'], playlistRatings['rating']))
+
+        add_playlist_ratings(playlistRatings)
+        return "Success"
     except DatabaseException as e:
         # Exception handling in case there is a database error.
         return redirect(frontend_url + "/error/database")
