125cfea5ba0c24c1223a3359bd55be32dc272f99
LucasFatas <69412382+LucasFatas@users.noreply.github.com>
2022-05-23 11:54:24 +0200

    Update retrieve scores to return data in csv file

--- backend/src/Controllers/DashboardController.py
+++ backend/src/Controllers/DashboardController.py
@@ -1,4 +1,5 @@
-from flask import request, Blueprint, jsonify
+import io, csv
+from flask import request, Blueprint, jsonify, make_response
 
 from src.Services.DashboardService import get_all_scores
 
@@ -11,6 +12,18 @@ def retrieve_scores():
 
     scores = get_all_scores(batchId)
 
-    print(scores)
+    data = io.StringIO()
+    writer = csv.writer(data)
+    column_names = ("UserID", "Openness", "Honesty", "Emotionality", "Extroversion", "Agreeableness", "Conscientiousness",
+                   "Stimulation", "SelfDirection", "Universalism", "Benevolence", "Tradition", "Conformity", "SecurityVal",
+                   "PowerVal", "Achievement", "Hedonism")
+    writer.writerow(column_names)
+    for row in scores:
+        writer.writerow(row)
+
+    output = make_response(data.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    output.headers["Content-type"] = "text/csv"
+    return output
 
 
