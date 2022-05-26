353aba6af0c1439a09c9c5016418f418ec412a31
Diego Viero <d.viero@student.tudelft.nl>
2022-05-26 12:18:25 +0200

    Implement batches logic in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,11 @@
+import { useState } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
 import { Questionnaire } from './components/questionnaire/Questionnaire';
 import { PageNotFound } from './components/errors/PageNotFound';
 import { ErrorRouter } from './components/errors/ErrorRouter';
@@ -7,19 +15,24 @@ import { Introduction } from './components/pages/introductions/Introduction';
 import { ConsentPage } from './components/pages/ConsentPage';
 import { Thanks } from './components/pages/Thanks';
 import { ResultPage } from './components/pages/ResultPage';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
 
 import * as data from './util/API.json'
 import intro from './util/introductions.json'
 
+import { switchBatch } from './controller/dashboardController';
+
+const defaultPage = '/loginPage';
+
 const App = () => {
 
-  const defaultPage = '/loginPage';
+  const [currentBatch, setCurrentBatch] = useState('questionnaire');
+
+  //Call this function to switch batch
+  /* 
+    TODO : once the dashboard is implemented, pass this function 
+    to the Dashboard component and call it from there.
+  */
+  const switchCurrentBatch = () => switchBatch(currentBatch, setCurrentBatch);
 
   return (
     <BrowserRouter>
@@ -33,7 +46,7 @@ const App = () => {
         <Route path="/introduction/personality" element={<Introduction intro={intro.personality} nextpage={'/questionnaire/p/page1'} />} />
         <Route path="/introduction/playlist" element={<Introduction intro={intro.playlist} nextpage={'/recommender'} />} />
         <Route path="/consentPage"  element={<ConsentPage defaultPage={defaultPage}/>} />
-        <Route path="/questionnaire/*" element={<Questionnaire defaultPage={defaultPage} />} />
+        <Route path="/questionnaire/*" element={<Questionnaire defaultPage={defaultPage} currentBatch={currentBatch}/>} />
         <Route path="/recommender/*" element={<Recommender  defaultPage={defaultPage}/>} />
         <Route path="/resultPage" element={<ResultPage/>} />
         <Route path="/thanks" element={<Thanks/>} />
--- frontend/src/controller/questionnaireController.js
+++ frontend/src/controller/questionnaireController.js
@@ -4,11 +4,13 @@ export const getRandomValue = (arr) => arr[Math.floor(Math.random() * arr.length
 
 export const checkEveryElementIsInMap = (array, map) => array.every(x => map.has(x));
 
+export const getLastPage = (currentBatch) => currentBatch === 'questionnaire' ? '/thanks' : '/introduction/playlist';
+
 /**
- * Splits the array into a 2d array based on the rowsize
+ * Splits the array into a 2D array based on the rowsize
  * @param {string[]} array array to be split in rows
  * @param {number} rowSize the row size
- * @returns a 2d array with the same content of the 
+ * @returns a 2D array with the same content of the 
  * original array but with the content split into rows.
  */
 export const splitArrayIntoMatrix = (array, rowSize) => {
@@ -51,7 +53,7 @@ export const stringifyAnswers = (answers) => {
 /**
  * Parses the session object into the answer object used in the useState
  * @param {{personality : string[][], values : string[][]}} obj 
- * object containing 2d array of size nx2 where n is
+ * object containing 2D array of size nx2 where n is
  * the number of questions and each item will become an entry
  * in a map where the first value is used as the key and the second as 
  * the value.
@@ -125,5 +127,29 @@ export const getRandomQuestionnaire = (options) => {
         return choice;
     } else 
         return selectedQuestionnaire;
-    
+}
+
+
+/**
+ * Returns data object based on parameters, this function is used both for values and personality.
+ * @param {object} obj obect imported from questions.JSON file, all the entries are spread and included in the returned object
+ * @param {string[][]} questionsMatrix 2D array containing questions text
+ * @param {string} type 'values' for values object and 'personality' for personality
+ * @param {string} lastPage last page path
+ * @param {string} firstQuestionnaire 'values' for values object and 'personality' for personality
+ * @returns data object containing useful information used in QuestionnairePage component.
+ */
+export const getDataObj = (obj, questionsMatrix, type, lastPage, firstQuestionnaire) => {
+
+    const nextQuestionnaire = firstQuestionnaire === 'values' ? 'personality' : 'values';
+
+    return {
+        ...obj,
+        matrix : questionsMatrix, 
+        pathOnSubmit : firstQuestionnaire === type ? `/introduction/${nextQuestionnaire}` : lastPage,
+        lastPage : questionsMatrix.length,
+        path : type === 'values' ? '/v' : '/p',
+        type : type,
+        submitResults : firstQuestionnaire === type
+    }
 }
