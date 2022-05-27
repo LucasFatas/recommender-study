ba09d91d53dea2851996a6f86260e8b359001f6c
Daniel Puente Barajas <D.PuenteBarajas@student.tudelft.nl>
2022-05-27 14:55:21 +0200

    Refactor match data method.
    Updated query and controller method to format the data in a more efficient way. We make use of temporary tables to ease the computation time that it takes to calculate each row, and concatenate the question answers in one row to reduce the return table size and computation complexity.
    Previous query combined all possible results in one query and created unnecessary duplicated results.
    Updated SQL query is in DashboardService.get_all_match_data and updated formatting method in DashboardController.retrieve_match_data.

--- backend/src/Controllers/DashboardController.py
+++ backend/src/Controllers/DashboardController.py
@@ -2,7 +2,7 @@ import io, csv
 import numpy as np
 from flask import request, Blueprint, jsonify, make_response
 
-from src.Services.DashboardService import get_all_scores, get_all_answers, get_all_songs, get_all_match_data
+from src.Services.DashboardService import get_all_scores, get_all_answers, get_all_songs, get_all_match_data, get_song_ratings
 
 dashboard = Blueprint('dashboard', __name__)
 
@@ -77,7 +77,6 @@ def retrieve_songs_from_batch():
 
 # Batch 2 information starts here.
 # Method that gets all the match information of users being matched.
-# Parameters: batch number
 # Returns: a csv of tuples containing userId and the feedback provided based on the matches they got.
 @dashboard.route("/match")
 def retrieve_match_data():
@@ -112,56 +111,19 @@ def retrieve_match_data():
     # Write the columns calculated to the CSV file.
     writer.writerow(tuple(column_names))
 
-    # Data computation starts here
-    i = 0
     # Will store the answers for all the questions across matches.
-    answers = np.zeros(number_of_questions*3)
     for row in match_data:
-        # We add entries to the answer array, checking all the combinations.
-        if i < pow(number_of_questions, 3) - 1:
-            # If all answers have been recorded, continue with the for loop, avoids unnecessary checks.
-            if answers.all():
-                i = i + 1
-                continue
-            # Record answers of first match.
-            if answers[row[3]-1] == 0:
-                answers[row[3] - 1] = row[4]
-            # Record answers of second match.
-            if answers[number_of_questions + row[8] - 1] == 0:
-                answers[number_of_questions + row[8]-1] = row[9]
-            # Record answers of third match.
-            if answers[2 * number_of_questions + row[13] - 1] == 0:
-                answers[2 * number_of_questions + row[13]-1] = row[14]
-            i = i + 1
+        final_row = [row[0], row[1], row[2]]
+        final_row.extend(row[3].split(","))
+        final_row.extend([row[4], row[5], row[6], row[7]])
+        final_row.extend(row[8].split(","))
+        final_row.extend([row[9], row[10], row[11], row[12]])
+        final_row.extend(row[13].split(","))
+        final_row.extend(row[14])
 
-        else:
-            # Add userId, matchedUserId and rating of first match (values).
-            final_row = [row[0], row[1], row[2]]
-            for answer in answers[0: number_of_questions]:
-                # Add answer for each question.
-                final_row.append(answer)
-
-            # Add feedback of first match, matchedUserId and rating of second match (personality).
-            final_row.extend([row[5], row[6], row[7]])
-            for answer in answers[number_of_questions: 2 * number_of_questions]:
-                # Add answer for each question.
-                final_row.append(answer)
-
-            # Add feedback of second match, matchedUserId and rating of third match (random).
-            final_row.extend([row[10], row[11], row[12]])
-            for answer in answers[2*number_of_questions:]:
-                # Add answer for each question.
-                final_row.append(answer)
-            final_row.append(row[15])
-
-            # Write the row after formatting correctly.
-            writer.writerow(final_row)
-            # Reset variables for handling of new match.
-            i = 0
-            answers = np.zeros(number_of_questions*3)
+        writer.writerow(final_row)
 
     output = make_response(data.getvalue())
     output.headers["Content-Disposition"] = "attachment; filename=export.csv"
     output.headers["Content-type"] = "text/csv"
     return output
-
--- backend/src/Services/DashboardService.py
+++ backend/src/Services/DashboardService.py
@@ -57,8 +57,7 @@ def get_all_songs(batch):
         raise DatabaseException("Error connecting to database when retrieving Scores.")
 
 
-# Method that gets all the songs of users of a certain batch
-# Parameters: batch number
+# Method that gets all the feedback (except individual songs) of users.
 # Returns: a list of tuples containing userId and for each match, the id of the matched user, the rating of the match
 # and the answer to each question about the match answered by the user.
 def get_all_match_data():
@@ -70,21 +69,31 @@ def get_all_match_data():
         cursor.execute(sql_questions)
         number_of_questions = cursor.fetchall()[0][0]
 
-        # Retrieve all data from the database based on the matches made by our app.
-        sql = """Select m.userId, 
-        m.valueId, pr1.rating, qf1.questionNumber, qf1.answer, of1.feedback, 
-        m.personalityId, pr2.rating, qf2.questionNumber, qf2.answer, of2.feedback, 
-        m.randomId, pr3.rating, qf3.questionNumber, qf3.answer, of3.feedback 
+        # Retrieve all data from the database based on the matches made by our app. We use three different temporary
+        # tables with the feedback from each match and then join them.
+        sql = """
+        with table1 as (
+            Select m.userId, m.valueId, pr1.rating, group_concat(qf1.answer order by qf1.questionNumber), of1.feedback
             from recommender.matches as m 
             join recommender.PlaylistRating as pr1 on pr1.userId = m.userId and pr1.matchedUserId = m.ValueId
             join recommender.QuestionFeedback as qf1 on qf1.userId = m.userId and qf1.matchedUserId = m.ValueId
-            join recommender.OpenFeedback as of1 on of1.userId = m.userId and of1.matchedUserId = m.ValueId 
+            join recommender.OpenFeedback as of1 on of1.userId = m.userId and of1.matchedUserId = m.ValueId)
+        , table2 as (
+            Select m.userId, m.personalityId, pr2.rating, group_concat(qf2.answer order by qf2.questionNumber), of2.feedback
+            from recommender.matches as m 
             join recommender.PlaylistRating as pr2 on pr2.userId = m.userId and pr2.matchedUserId = m.personalityId 
             join recommender.QuestionFeedback as qf2 on qf2.userId = m.userId and qf2.matchedUserId = m.personalityId 
-            join recommender.OpenFeedback as of2 on of2.userId = m.userId and of2.matchedUserId = m.personalityId 
-            join recommender.PlaylistRating as pr3 on pr3.userId = m.userId and pr3.matchedUserId = m.randomId
+            join recommender.OpenFeedback as of2 on of2.userId = m.userId and of2.matchedUserId = m.personalityId)
+        , table3 as (
+            Select m.userId, m.randomId, pr3.rating, group_concat(qf3.answer order by qf3.questionNumber), of3.feedback 
+            from recommender.matches as m 
+            join recommender.PlaylistRating as pr3 on pr3.userId = m.userId and pr3.matchedUserId = m.randomId 
             join recommender.QuestionFeedback as qf3 on qf3.userId = m.userId and qf3.matchedUserId = m.randomId 
-            join recommender.OpenFeedback as of3 on of3.userId = m.userId and of3.matchedUserId = m.randomId
+            join recommender.OpenFeedback as of3 on of3.userId = m.userId and of3.matchedUserId = m.randomId)
+            
+        select * from table1 
+            join table2 on table1.userId = table2.userId
+            join table3 on table1.userId = table3.userId
         """
 
         cursor.execute(sql)
@@ -93,4 +102,4 @@ def get_all_match_data():
 
     except mysql.connector.errors.Error as e:
         print(e)
-        raise DatabaseException("Error connecting to database when retrieving Scores.")
+        raise DatabaseException("Error connecting to database when retrieving match feedback")
