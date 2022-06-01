6fc3355be24c0f550dd3a023cf46ba7e59586733
Diego Viero <d.viero@student.tudelft.nl>
2022-06-01 14:32:23 +0200

    Implement show legend with labels on values questionnaire
    CHanges : 
    - Values questionnaire now has legend with answers labels and what they mean
    - Agree/Disagree is not shown anymore on values questionnaire
    - Changed "strongly disagree/agree" to "disagree/agree" in other questionnaires
    - Display real questions in feedback part

--- frontend/src/components/global/RadioButton.jsx
+++ frontend/src/components/global/RadioButton.jsx
@@ -1,4 +1,4 @@
-const inputStyle = "appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer";
+const inputStyle = "appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mx-1 cursor-pointer";
 
 export const RadioButton = (props) => {
 
@@ -11,13 +11,20 @@ export const RadioButton = (props) => {
   } = props;
 
   return (
-    <input 
-      type="radio" 
-      className={inputStyle} 
-      value={value}
-      name={`${questionNumber}${type}`}
-      onChange={e => onChange(e, questionNumber)}
-      defaultChecked={answers.get(questionNumber + 1) === value}
-    /> 
+    <div className="flex flex-col items-center">
+      {type === 'values' 
+        ? <label className="" htmlFor={`${questionNumber}${type}${value}`}>{value}</label>
+        : ""
+      }
+      <input 
+        type="radio" 
+        className={inputStyle} 
+        value={value}
+        id={`${questionNumber}${type}${value}`}
+        name={`${questionNumber}${type}`}
+        onChange={e => onChange(e, questionNumber)}
+        defaultChecked={answers.get(questionNumber + 1) === value}
+      /> 
+    </div>
   )
 }
