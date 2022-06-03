9a8599ae348ce93084e94203549b0aa4f04a9a9d
Nathaniël De Leeuw <N.DeLeeuw@student.tudelft.nl>
2022-06-03 17:05:37 +0200

    Refactor API communication

--- frontend/src/API/Dashboard.js
+++ frontend/src/API/Dashboard.js
@@ -1,93 +1,58 @@
 const { serverUrl, port } = require('../util/API.json');
 
 
-export const getSongs = async (batchId, token) => {
+const retrieveCSVHelperFunction = async ({batchId, token, endpoint}) => {
     console.log(batchId);
     try {
-        const response = await fetch(`${serverUrl}:${port}/dashboard/songs?batchId=${batchId}`, {
+        if (typeof batchId === 'undefined' || batchId === null){
+            const response = await fetch(`${serverUrl}:${port}/dashboard/${endpoint}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': "Bearer " + token,
+                    'Content-Type': 'text/csv'
+                    
+                }
+            }).then(res => res.text());
+            console.log(response);
+            return response
+        }else{
+            
+            const response = await fetch(`${serverUrl}:${port}/dashboard/${endpoint}?batchId=${batchId}`, {
             method: 'GET',
             headers: {
                 'Authorization': "Bearer " + token,
                 'Content-Type': 'text/csv'
                 
             }
-        }).then(res => res.text());
-        console.log(response);
-        return response
+            }).then(res => res.text());
+            console.log(response);
+            return response
+            
+        }
+        
     } catch (error) {
         console.log(error);
     }
+}
+
+export const getSongs = async (batchId, token) => {
+    return retrieveCSVHelperFunction({batchId : batchId, token : token, endpoint : "songs"})
 }
 
 export const getSongRatings = async (token) => {
-    console.log("ratings")
-    try {
-        const response = await fetch(`${serverUrl}:${port}/dashboard/songRatings`, {
-            method: 'GET',
-            headers: {
-                'Authorization': "Bearer " + token,
-                'Content-Type': 'text/csv'
-                
-            }
-        }).then(res => res.text());
-        console.log(response);
-        return response
-    } catch (error) {
-        console.log(error);
-    }
+   return retrieveCSVHelperFunction({token : token, endpoint : "songRatings"})
 }
 
 export const getScores = async (batchId, token) => {
-    console.log(batchId);
-    try {
-        const response = await fetch(`${serverUrl}:${port}/dashboard/scores?batchId=${batchId}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': "Bearer " + token,
-                'Content-Type': 'text/csv'
-                
-            }
-        }).then(res => res.text());
-        console.log(response);
-        return response
-    } catch (error) {
-        console.log(error);
-    }
+   return retrieveCSVHelperFunction({token : token, batchId : batchId, endpoint : "scores"})
 }
 
 export const getAnswers = async (batchId, token) => {
-    console.log(batchId);
-    try {
-        const response = await fetch(`${serverUrl}:${port}/dashboard/answers?batchId=${batchId}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': "Bearer " + token,
-                'Content-Type': 'text/csv'
-                
-            }
-        }).then(res => res.text());
-        console.log(response);
-        return response
-    } catch (error) {
-        console.log(error);
-    }
+    return retrieveCSVHelperFunction({token : token, batchId : batchId, endpoint : "answers"})
 }
 
 export const getMatchData = async (token) => {
-    try {
-        const response = await fetch(`${serverUrl}:${port}/dashboard/match`, {
-            method: 'GET',
-            headers: {
-                'Authorization': "Bearer " + token,
-                'Content-Type': 'text/csv'
-                
-            }
-        }).then(res => res.text());
-        console.log(response);
-        return response
-    } catch (error) {
-        console.log(error);
-    }
+    return retrieveCSVHelperFunction({token : token, endpoint : "match"})
 }
 
 
