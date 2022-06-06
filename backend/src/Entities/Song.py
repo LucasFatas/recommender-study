9b41bae5cd0ec6edaeee5f2fb2d92a09443472b6
Daniel Puente Barajas <D.PuenteBarajas@student.tudelft.nl>
2022-06-06 23:19:22 +0200

    Add docstring to entities.
    They all have been provided an explanation on why we created them and what they store and represent.

--- backend/src/Entities/Song.py
+++ backend/src/Entities/Song.py
@@ -1,6 +1,13 @@
 class Song:
 
     def __init__(self, preview_url, name, artists, spotify_url):
+        """
+        Entity that stores all relevant information about the songs fetched from Spotify
+        :param preview_url: URL that sends you to a MP3 30-second snippet of the song
+        :param name: name of the song
+        :param artists: list with artists of the song
+        :param spotify_url: Official Spotify URL that identifies the song within this service
+        """
         self.artists = artists
         self.name = name
         self.spotify_url = spotify_url
