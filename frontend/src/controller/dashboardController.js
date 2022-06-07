87d83aa2e4d53e93c2dc25c58a9c69fbc4cab340
Nathaniël De Leeuw <N.DeLeeuw@student.tudelft.nl>
2022-06-07 12:05:57 +0200

    Remove non-used function switchdashboard

--- frontend/src/controller/dashboardController.js
+++ frontend/src/controller/dashboardController.js
@@ -1,16 +1,6 @@
 import { getAnswers, getMatchData, getScores, getSongRatings, getSongs } from "../API/Dashboard";
 
 
-/**
- * If batch is set to 'questionnaire', set it to 'recommender and vice versa
- * @param {string} currentBatch either 'questionnaire' or 'recommender'
- * @param {*} setCurrentBatch function to change current batch
- */
-export const switchBatch = (currentBatch, setCurrentBatch) => {
-    currentBatch === 'questionnaire' 
-        ? setCurrentBatch('recommender') 
-        : setCurrentBatch('questionnaire');
-}
 
 /**
  * Check if the user is logged in, if not, she/he is redirected to the log in
