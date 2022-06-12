1461f0454a95274009ce359fbf5c8f3849fa7f0e
Diego Viero <d.viero@student.tudelft.nl>
2022-06-12 19:08:09 +0200

    Fix typo

--- frontend/src/components/dashboard/LogIn.jsx
+++ frontend/src/components/dashboard/LogIn.jsx
@@ -51,7 +51,7 @@ export const LogIn = (props) => {
           focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" type="text" onChange={e => setUserName(e.target.value)}/>
         </label>
         <label className="flex items-center pb-8">
-          <input placeholder="Username" className="mt-1 block  px-6 py-3 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+          <input placeholder="Password" className="mt-1 block  px-6 py-3 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" type="password" onChange={e => setPassword(e.target.value)}/>
         </label>
         <div className="text-center">
